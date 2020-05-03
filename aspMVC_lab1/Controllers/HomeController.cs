f976c78a1b9ab1eb77bfe1d48cbe04ecbef11ad0
AhmedAbbas19 <ahmeddabbas19@yahoo.com>
2020-05-03 16:17:40 +0200

    init commit

---
+++ aspMVC_lab1/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace aspMVC_lab1.Controllers
+{
+
+    public class User {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+    }
+
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ViewResult RsvpForm()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ViewResult RsvpForm(User user) {
+            if (user != null && user.Name != null) {
+                ViewData["name"] = user.Name;
+                return View("Welcome");
+            }
+                return View();
+        }
+    }
+}
---
+++ aspMVC_lab1/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="aspMVC_lab1.MvcApplication" Language="C#" %>
